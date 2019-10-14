865317396d768b18f113a84f8b7e835d7da65337
chrismjohnston <cmjohnston@gmail.com>
2019-10-14 18:00:59 -0500

    got some test working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Controls from './Controls';
+
+
+describe("<Controls />", () => {
+    it("button is disabled", () => {
+        const { getByText, getByTestId, container } = render(
+            <Controls locked={true} closed={true} />
+        );
+
+        const disabledButton = getByText(/disabled/i);
+
+
+        expect(getByTestId(container, disabledButton)).toBeDisabled();
+
+        expect(disabledButton).toHaveAttribute("disabled");
+    });
+
+    it("unmounts component after each test", () => {
+        console.log(document.body.outerHTML);
+    });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,13 +1,44 @@
 // Test away
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 import Dashboard from './Dashboard';
 
-describe('<Dashboard />', () => {
-    it('should match <Dashboard /> snapshot', () => {
-        const tree = renderer.create(<Dashboard />).toJSON();
-
-        expect(tree).toMatchSnapshot();
+describe("<Dashboard />", () => {
+    it("renders close, open and lock, unlock status", () => {
+      const { getAllByText, getByText, debug } = render(<Dashboard />);
+      debug();
+  
+      getAllByText(/lock/i);
+      getByText(/close/i);
+      getByText(/open/i);
+      getByText(/unlocked/i);
     });
-}); 
+  
+    it("renders the display", () => {
+      const { getByTestId } = render(<Dashboard />);
+      const display = getByTestId("display");
+      expect(display).toHaveClass("display panel");
+    });
+  
+    it("renders the controls", () => {
+      const { getByTestId } = render(<Dashboard />);
+      const controls = getByTestId("controls");
+      expect(controls).toHaveClass("controls panel");
+    });
+  
+    it("renders display and controls", () => {
+      const { getByTestId } = render(<Dashboard />);
+  
+      const display = getByTestId("display");
+      const controls = getByTestId("controls");
+  
+      expect(display).toBeInTheDocument();
+      expect(controls).toBeInTheDocument();
+    });
+  
+    it("unmounts component after each test", () => {
+      console.log(document.body.outerHTML);
+    });
+  });
