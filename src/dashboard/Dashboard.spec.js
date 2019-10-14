ea6c2600775e4e0754fefee9c414df86bb66f556
chrismjohnston <cmjohnston@gmail.com>
2019-10-14 18:22:50 -0500

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,38 +7,38 @@ import Dashboard from './Dashboard';
 
 describe("<Dashboard />", () => {
     it("renders close, open and lock, unlock status", () => {
-      const { getAllByText, getByText, debug } = render(<Dashboard />);
-      debug();
-  
-      getAllByText(/lock/i);
-      getByText(/close/i);
-      getByText(/open/i);
-      getByText(/unlocked/i);
+        const { getAllByText, getByText, debug } = render(<Dashboard />);
+        debug();
+
+        getAllByText(/lock/i);
+        getByText(/close/i);
+        getByText(/open/i);
+        getByText(/unlocked/i);
     });
-  
+
     it("renders the display", () => {
-      const { getByTestId } = render(<Dashboard />);
-      const display = getByTestId("display");
-      expect(display).toHaveClass("display panel");
+        const { getByTestId } = render(<Dashboard />);
+        const display = getByTestId("display");
+        expect(display).toHaveClass("display panel");
     });
-  
+
     it("renders the controls", () => {
-      const { getByTestId } = render(<Dashboard />);
-      const controls = getByTestId("controls");
-      expect(controls).toHaveClass("controls panel");
+        const { getByTestId } = render(<Dashboard />);
+        const controls = getByTestId("controls");
+        expect(controls).toHaveClass("controls panel");
     });
-  
+
     it("renders display and controls", () => {
-      const { getByTestId } = render(<Dashboard />);
-  
-      const display = getByTestId("display");
-      const controls = getByTestId("controls");
-  
-      expect(display).toBeInTheDocument();
-      expect(controls).toBeInTheDocument();
+        const { getByTestId } = render(<Dashboard />);
+
+        const display = getByTestId("display");
+        const controls = getByTestId("controls");
+
+        expect(display).toBeInTheDocument();
+        expect(controls).toBeInTheDocument();
     });
-  
+
     it("unmounts component after each test", () => {
-      console.log(document.body.outerHTML);
+        console.log(document.body.outerHTML);
     });
-  });
+});
